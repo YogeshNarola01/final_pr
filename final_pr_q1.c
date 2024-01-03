39eaae7ffec185ead48515711aa0b832559b3ca7
YOGESH NAROLA <133344131+YogeshNarola01@users.noreply.github.com>
2024-01-03 17:23:25 +0530

    yogeshnarola01

--- final_pr_q1.c
+++ final_pr_q1.c
@@ -2,18 +2,19 @@
 Q.1 WAP to print all odd numbers from a given range using UDF.
 */
 #include<stdio.h>
-
+int Odd(int a){
+	return a;
+}
 void main(){
-int n;
-int num=1;
-
-printf("Enter a Number: ");
-scanf("%d",&n);
-
-if(n%2==0){
-	printf("%d\n",num);
-	num++;
+	int n,m;
+	printf("Enter Number 1 : ");
+	scanf("%d",&n);
+	printf("Enter Number 2 : ");	
+	scanf("%d",&m);
+	while(n<){
+		if(n%2!=0){
+		printf(" %d ",Odd(n));
+		}
+	n++;
+	}
 }
-
-}
-
