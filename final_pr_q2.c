156683a9cefd5ad34ae41f4c42452068f9753640
YOGESH NAROLA <133344131+YogeshNarola01@users.noreply.github.com>
2024-01-03 17:30:23 +0530

    yogeshnarola01

--- final_pr_q2.c
+++ final_pr_q2.c
@@ -4,24 +4,28 @@ Q.2 WAP to find a cube of all elements from a given 2D array.
 
 #include<stdio.h>
 void main(){
-	int n;
-	int a[n][n];
+	int y,k;
+	int a[y][k];
 	int i,j;
+	printf("Enter the size of Rows : ");
+	scanf("%d",&y);
+	printf("Enter the Size of Columns : ");
+	scanf("%d",&k);
 	
-	printf("Enter array size : ");
-	scanf("%d",&n);
-	
-	for(i=0; i<n; i++){
-		for(j=0; j<n; j++){
-			printf("Enter array value : ");
-			scanf("%d",&a[i][j]);
+	for(i=0; i<y; i++){
+		for(j=0; j<k; j++){
+			printf("Enter Element for A[%d][%d] : ",i,j);
+			scanf("%d \n",&a[i][j]);
 		}
 	}
-	
-	for(i=0; i<n; i++){
-		printf("Cube Is %d: \n",(a[i][j])*(a[i][j])*(a[i][j]));		
+	for(i=0; i<y; i++){
+		for(j=0; j<k; j++){
+			printf("%d \n",a[i][j]);
+		}
 	}
-	for(j=0; j<n; j++){
-		printf("Cube Is %d: \n",(a[i][j]*a[i][j]*a[i][j]));
+	for(i=0; i<y; i++){
+		for(j=0; j<k; j++){
+			printf("%d \n",a[i][j]*a[i][j]*a[i][j]);
+		}
 	}
 }
