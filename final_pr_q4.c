feb2a429a4b1d7e4c62a0e6b1d5598116ccf30f0
YOGESH NAROLA <133344131+YogeshNarola01@users.noreply.github.com>
2024-01-03 17:36:00 +0530

    yogeshnarola01

--- final_pr_q4.c
+++ final_pr_q4.c
@@ -2,16 +2,26 @@
 Q.4 WAP to print all odd numbers from a given 1D array using Pointer.
 */
 #include<stdio.h>
-
  main(){
-	int i,n,odd=1;
+	int i,n;
 	int a[n];
 	int *p;
 	p = &a;
-	
 	printf("Enter any number : ");
 	scanf("%d",&n);
-	
+	for(i=0;i<n;i++){
+		printf("Enter Element for a[%d] : ",i);
+		scanf("%d \n",&a[i]);
+	}
+	for(i=0;i<n;i++){
+		printf("%d \n",a[i]);
+	}
+	for(i=0;i<n;i++){
+		if(*p[i]%2!=0){
+			a[i]=*p[i];
+			printf("%d \n",*p[i]);
+		}
+	}	
 }
 	
 
